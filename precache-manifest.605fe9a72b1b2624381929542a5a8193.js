a8f6f9a787dbc2b0f1822dcac068b16033eedcf1
Vinicius Torcate Santini <vinicius2000.santini@hotmail.com>
2020-08-28 17:07:28 -0300

    Updates

---
+++ precache-manifest.605fe9a72b1b2624381929542a5a8193.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd2e249230d9fb5acc17febe112f3942",
+    "url": "/GetPizza/index.html"
+  },
+  {
+    "revision": "caca245c91d1cc856779",
+    "url": "/GetPizza/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3b6eb56a4d0ab3e5cb4d",
+    "url": "/GetPizza/static/css/main.0af72bf6.chunk.css"
+  },
+  {
+    "revision": "caca245c91d1cc856779",
+    "url": "/GetPizza/static/js/2.bfbb7062.chunk.js"
+  },
+  {
+    "revision": "b25e0c44d0b24745f7d62e2ea6be31e5",
+    "url": "/GetPizza/static/js/2.bfbb7062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b6eb56a4d0ab3e5cb4d",
+    "url": "/GetPizza/static/js/main.f45b71dd.chunk.js"
+  },
+  {
+    "revision": "a7e4500d0b8f0161c05c",
+    "url": "/GetPizza/static/js/runtime-main.7d757276.js"
+  },
+  {
+    "revision": "c5022d449c49328a1268a1765a252db0",
+    "url": "/GetPizza/static/media/pizza-logo.c5022d44.svg"
+  }
+]);
